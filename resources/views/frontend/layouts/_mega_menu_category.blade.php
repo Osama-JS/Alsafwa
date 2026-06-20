4449db22d48103ebbda8cf0427de616f5534321f
Osama-JS <osama.samomy@gmail.com>
2026-06-20 18:36:52 +0300

    save sections

--- resources/views/frontend/layouts/_mega_menu_category.blade.php
+++ resources/views/frontend/layouts/_mega_menu_category.blade.php
@@ -1,9 +1,17 @@
-<li class="mb-1">
-    <a href="{{ route('products.index', ['category_id[]' => $category->id, 'navbar' => 1]) }}" class="text-xs text-secondary opacity-75 hover-primary text-decoration-none transition-03 d-block py-1">
-        - {{ $category->{'name_' . app()->getLocale()} }}
-    </a>
+<li class="mb-2">
+    <div class="d-flex align-items-center justify-content-between transition-03 py-1">
+        <a href="{{ route('products.index', ['category_id[]' => $category->id, 'navbar' => 1]) }}" 
+           class="text-sm text-secondary hover-primary text-decoration-none flex-grow-1">
+            <span class="fw-medium">{{ $category->{'name_' . app()->getLocale()} }}</span>
+        </a>
+        @if($category->allChildren && $category->allChildren->count() > 0)
+            <button class="btn btn-sm btn-link text-secondary opacity-50 p-0 ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#megaCat-{{ $category->id }}" aria-expanded="false" onclick="this.querySelector('i').classList.toggle('fa-chevron-down'); this.querySelector('i').classList.toggle('fa-chevron-up');">
+                <i class="fas fa-chevron-down" style="font-size: 0.8em;"></i>
+            </button>
+        @endif
+    </div>
     @if($category->allChildren && $category->allChildren->count() > 0)
-        <ul class="list-unstyled p-0 m-0 ps-3 ms-1 border-start border-light border-opacity-10">
+        <ul class="collapse list-unstyled p-0 m-0 ps-3 mt-1 border-start border-secondary border-opacity-25" id="megaCat-{{ $category->id }}">
             @foreach($category->allChildren as $child)
                 @include('frontend.layouts._mega_menu_category', ['category' => $child])
             @endforeach
--- resources/views/frontend/layouts/_mobile_menu_category.blade.php
+++ resources/views/frontend/layouts/_mobile_menu_category.blade.php
@@ -1,9 +1,17 @@
-<div class="mb-1">
-    <a href="{{ route('products.index', ['category_id[]' => $category->id, 'navbar' => 1]) }}" class="py-1 text-xs opacity-75 d-block text-decoration-none">
-        - {{ $category->{'name_' . app()->getLocale()} }}
-    </a>
+<div class="mb-2">
+    <div class="d-flex align-items-center justify-content-between py-2 border-bottom border-light border-opacity-10 transition-03">
+        <a href="{{ route('products.index', ['category_id[]' => $category->id, 'navbar' => 1]) }}" 
+           class="text-sm text-white opacity-75 hover-primary text-decoration-none flex-grow-1">
+            <span class="fw-medium">{{ $category->{'name_' . app()->getLocale()} }}</span>
+        </a>
+        @if($category->allChildren && $category->allChildren->count() > 0)
+            <button class="btn btn-sm btn-link text-white opacity-50 p-0 ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#mobileCat-{{ $category->id }}" aria-expanded="false" onclick="this.querySelector('i').classList.toggle('fa-chevron-down'); this.querySelector('i').classList.toggle('fa-chevron-up');">
+                <i class="fas fa-chevron-down" style="font-size: 0.8em;"></i>
+            </button>
+        @endif
+    </div>
     @if($category->allChildren && $category->allChildren->count() > 0)
-        <div class="ps-3 border-start ms-2">
+        <div class="collapse ps-3 border-start border-light border-opacity-25 mt-1 ms-2" id="mobileCat-{{ $category->id }}">
             @foreach($category->allChildren as $child)
                 @include('frontend.layouts._mobile_menu_category', ['category' => $child])
             @endforeach
